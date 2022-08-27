a5b30798417c96541872e1f8239fe4ef9541021b
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2022-08-27 10:55:55 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var num1=prompt("Enter your first number:")
+
+var num2=prompt("Enter your second number:")
+
+var sum,sub,kol,mol
+
+sum = num+num2;
+sub = num1-num2;
+kol = num1*num2;
+mol = num1%num2;
+
+document.write("Plus =" + sum+ "<br>")
+document.write("Minus =" + sub + "<br>")
+document.write("intu =" + sum + "<br>")
+document.write("Parsent =" + sum )
+
+
